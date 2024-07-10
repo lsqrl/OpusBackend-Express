9e4d3ecea798a0260700c8e24291bd2f0a18d785
lsqrl <lsqrl91@gmail.com>
2024-07-10 17:32:48 +0200

    calculate balance

--- database/api/__init__.py
+++ database/api/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from config import Config
+from .config import Config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
--- database/api/routes.py
+++ database/api/routes.py
@@ -1,19 +1,7 @@
 from flask import jsonify, request, current_app as app, abort
 from data_types import Currency, LiquidityPool, Users, LiquidityProviderAccountTrans
 
-@app.route('/currencies', methods=['GET'])
-def get_currencies():
-    session = app.session
-    currencies = session.query(Currency).all()
-    return jsonify([currency.to_dict() for currency in currencies])
-
-@app.route('/users', methods=['GET'])
-def get_users():
-    session = app.session
-    users = session.query(Users).all()
-    return jsonify([user.to_dict() for user in users])
-
-@app.route('/insertUser', methods=['POST'])
+@app.route('/newUser', methods=['POST'])
 def create_user():
     data = request.get_json()
 
@@ -40,46 +28,42 @@ def create_user():
         session.rollback()
         abort(400, description=f"An error occurred: {str(e)}")
 
-@app.route('/lpptransaction', methods=['POST'])
+@app.route('/newLPPTransaction', methods=['POST'])
 def create_liquidity_pool_transaction():
     data = request.get_json()
-
-    print(data)
 
     if not data:
         abort(400, description="No input data provided")
 
     try:
-        print("trying!")
         new_lppt = LiquidityProviderAccountTrans(
             user_id=data['user_id'],
             transaction_type=data['transaction_type'],
             amount=data['amount']
         )
-        print(new_lppt)
         session = app.session()
-        print(session)
         session.add(new_lppt)
-        print(session)
         session.commit()
-        print("committed!")
         return jsonify(new_lppt.to_dict()), 201
     except Exception as e:
-        print("error!")
-        print(e)
         session.rollback()
         abort(400, description=f"An error occurred: {str(e)}")
 
-@app.route('/liquidityPools', methods=['GET'])
-def get_liquidity_pools():
-    session = app.session
-    pools = session.query(LiquidityPool).all()
-    return jsonify([pool.to_dict() for pool in pools])
+@app.route('/calculateLPBalance/<int:user_id>', methods=['GET'])
+def calculate_lp_balance(user_id):
+    try:
+        session = app.session()
+        lp_balance = session.query(LiquidityProviderAccountTrans).filter_by(user_id=user_id).all()
+        total_balance = 0
+        for balance in lp_balance:
+            if balance.transaction_type == 'external_deposit' or balance.transaction_type == 'internal_deposit':
+                total_balance += balance.amount
+            elif balance.transaction_type == 'external_withdrawal' or balance.transaction_type == 'internal_withdrawal':
+                total_balance -= balance.amount
+        return jsonify({"balance": total_balance}), 200
+    except Exception as e:
+        abort(400, description=f"An error occurred: {str(e)}")
 
-@app.route('/currencies/<string:abbreviation>', methods=['GET'])
-def get_currency(abbreviation):
-    session = app.session()
-    currency = session.query(Currency).filter_by(abbreviation=abbreviation).first()
-    if not currency:
-        abort(404, description="Currency not found")
-    return jsonify(currency.to_dict())
+@app.route('/checkConnection', methods=['GET'])
+def check_connection():
+    return jsonify({"message": "Connection successful!"}), 200
